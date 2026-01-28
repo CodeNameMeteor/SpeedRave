ec60d9c3bb2b47541610b004ac81de04464ce166
Dylan <119975251+CodeNameMeteor@users.noreply.github.com>
2026-01-28 15:38:22 +0000

    Better Multiple Food Control Fix

--- Source/ReferenceManager.cs
+++ Source/ReferenceManager.cs
@@ -41,6 +41,17 @@ namespace SpeedRave
         private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             RefreshReferences();
+            if(scene.name.ToLower() == "sewer_start")
+            {
+                var foodControls = UnityEngine.Object.FindObjectsOfType<FoodControl>();
+
+                //The way the food_control is programmed is that it is initialised on the start of sewer_start
+                //in a typical game you can't go back to sewer_start but as we can just have this here as a precaution
+                if (foodControls.Length > 1)
+                {
+                    UnityEngine.Object.Destroy(UnityEngine.Object.FindObjectsOfType<FoodControl>()[1]);
+                }
+            }
         }
 
         private static void RefreshReferences()
@@ -59,9 +70,11 @@ namespace SpeedRave
             if(ActiveInventory != null)
             {
                 ActiveFoodControl = ActiveInventory.GetComponent<FoodControl>();
-
+                /*
                 var foodControls = UnityEngine.Object.FindObjectsOfType<FoodControl>();
 
+                //The way the player is programmed is that it is initialised on the start of sewer_start
+                //in a typical game you can't go back to sewer_start but as we can just have this here as a precaution
                 if (foodControls.Length > 1)
                 {
                     for (int i = foodControls.Length - 1; i > 0; i--)
@@ -69,6 +82,7 @@ namespace SpeedRave
                         UnityEngine.Object.Destroy(foodControls[i]);
                     }
                 }
+                */
             }
             else
             {
